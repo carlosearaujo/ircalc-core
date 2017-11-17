2d53d8e2abf6add9c64e626de621d643206d1767
Carlos E. Araujo <carlos.araujo@mv.com.br>
2017-11-17 10:11:55 -0300

    Initial commit

---
+++ src/main/java/com/ircalc/ircalccore/IrcalcCoreApplication.java
@@ -0,0 +1,12 @@
+package com.ircalc.ircalccore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrcalcCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrcalcCoreApplication.class, args);
+	}
+}
